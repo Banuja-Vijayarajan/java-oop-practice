723637218aabf79890fb0bad75e758af1e70eae7
Banu <vbanuja22@gmail.com>
2025-07-23 14:55:23 +0530

    Add files via upload

--- beginner/Car.java
+++ beginner/Car.java
@@ -1,1 +1,22 @@
-
+public class Car{
+    String brand;
+    String model;
+    int year;
+    
+    Car(String b,String m,int y){
+        this.brand=b;
+        this.model=m;
+        this.year=y;
+    }
+    
+    void displayDetails(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+        
+    public static void main(String[] args){
+    Car car = new Car("Toyota","Corolla",2010);
+    car.displayDetails();
+    }
+}
